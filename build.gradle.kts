6de9c58b3d2f69d597675668d2ef7c32d0a41ac3
Project_IO <me@projecttl.net>
2023-05-31 23:05:43 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.10"
+    kotlin("jvm") version "1.8.21"
 }
 
 group = property("group")!!
@@ -19,13 +19,13 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
-    implementation("io.github.monun:kommand-api:3.1.2")
-    implementation("net.projecttl:InventoryGUI-api:4.4.0")
+    implementation("io.github.monun:kommand-api:3.1.3")
+    implementation("net.projecttl:InventoryGUI-api:4.4.1")
     implementation("org.jetbrains.exposed:exposed-core:0.41.1")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1")
 
     compileOnly("org.mariadb.jdbc:mariadb-java-client:3.1.2")
-    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
 }
 
 tasks {
